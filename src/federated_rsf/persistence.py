2a3dac5dcbaa51aef246ca3a7b5db692cf619e7e
JonasHri <jonasharriehausen@gmail.com>
2026-02-18 11:11:32 +0100

    added option to create parent directories on save

--- src/federated_rsf/persistence.py
+++ src/federated_rsf/persistence.py
@@ -1,30 +1,37 @@
 from os import PathLike
 from typing import Self
+from pathlib import Path
 import joblib
 
 
 class SaveLoadMixin:
     """Save and load functionality for classes of FederatedRandomSurvivalForest Package."""
 
-    def save(self, path: PathLike):
+    def save(self, path: PathLike, create_folders: bool=False):
         """
-        Preferred method to save the model to a file.
+        Preferred method to save the object to a file.
 
         Parameters
         ----------
         path : PathLike
-            The file path to save the model to.
+            The file path to save the object to.
+        create_folders: Bool
+            Whether to create parent folders if they do not exist already.
         """
+
+        if create_folders:
+            Path(path).mkdir(parents=True, exist_ok=True)
+
         joblib.dump(self, path)
 
     @classmethod
     def load(cls, path: PathLike) -> Self:
         """
-        Preferred method to load the model from a file.
+        Preferred method to load the object from a file.
 
         Parameters
         ----------
         path : PathLike
-            The file path to load the model from.
+            The file path to load the object from.
         """
         return joblib.load(path)
